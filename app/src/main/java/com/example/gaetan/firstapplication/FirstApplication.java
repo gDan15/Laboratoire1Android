73448161b62cccd58b048db49c56044205f386cc
Gaétan Guru <gaetan.guru@gmail.com>
2018-02-20 20:44:59 +0100

    Second commit

--- app/src/main/java/com/example/gaetan/firstapplication/FirstApplication.java
+++ app/src/main/java/com/example/gaetan/firstapplication/FirstApplication.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -92,3 +96,43 @@ public class FirstApplication extends AppCompatActivity {
         new QueryTask().execute("https://andfun-weather.udacity.com/weather");
     }
 }
+public class ItemAdapter
+        extends RecyclerView.Adapter<ItemAdapter.ItemAdapterViewHolder> {
+        private String[] mData = null;
+        public ItemAdapter() {
+        }
+        public class ItemAdapterViewHolder
+            extends RecyclerView.ViewHolder {
+            public final TextView mTextView;
+            public ItemAdapterViewHolder(View view) {
+                super(view);
+                mTextView = (TextView) view.findViewById(R.id.data);
+            }
+        }
+        @Override
+        public ItemAdapterViewHolder onCreateViewHolder
+                (ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
+            View view = inflater.inflate(layoutIdForListItem,
+                    viewGroup, shouldAttachToParentImmediately);
+            return new ItemAdapterViewHolder(view);
+        }
+        @Override
+        public void onBindViewHolder
+                (ItemAdapterViewHolder itemAdapterViewHolder, int position){
+        String dataForThisItem = mData[position];
+        itemAdapterViewHolder.mTextView.setText(dataForThisItem);
+        }
+        @Override
+        public int getItemCount() {
+            if (null == mData) return 0;
+            return mData.length;
+        }
+        public void setData(String[] data) {
+            mData = data;
+            notifyDataSetChanged();
+        }
+    }
